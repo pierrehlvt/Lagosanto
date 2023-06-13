2e03d0930e2f79b39da5d2fc0f096cd5486cd9b1
Julien76140 <58588439+Julien76140@users.noreply.github.com>
2023-06-13 23:11:24 +0200

    add condition to loginview

--- Lagosanto/App.xaml.cs
+++ Lagosanto/App.xaml.cs
@@ -2,21 +2,22 @@
 using System.Windows;
 using Lagosanto.Views;
 
-
 namespace Lagosanto
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App: Application
     {
         private void ApplicationStart(object sender, StartupEventArgs e)
         {
             
              var loginView = new LoginWindowView();
              loginView.Show();
-             loginView.IsVisibleChanged += (sender, e) =>
+             loginView.IsVisibleChanged += (_, _) =>
              {
+                 if (loginView.IsVisible || !loginView.IsLoaded) return;
+                 
                  Dispatcher.BeginInvoke(new Action(() =>
                  {
                      Window mainView = new MainWindowView();
